12adf1d8086f733ab87128249bf2ba3beda3e1d1
Yined Molina <yined.molina@gmail.com>
2024-01-04 20:22:55 -0500

    adaptation api to use factory

--- applications/SIP/modules/services/api_services/api_attendances.py
+++ applications/SIP/modules/services/api_services/api_attendances.py
@@ -1,47 +1,36 @@
+from applications.SIP.modules.factory.attendance_factory import AttendanceFactory
+
 class APIAttendance:
     def __init__(self, db):
         self.db = db
-
+        self.factory = AttendanceFactory(db)
+    
     def create_attendance(self, attendance_data):
         try:
-            attendance_id = self.db.attendance.insert(**attendance_data)
-            self.db.commit()
-            return {'status': 'success', 'attendance_id': attendance_id, 'http_status': 201}
+            attendance = self.factory.get_or_create_attendance(attendance_data)
+            return {'status': 'success', 'attendance_id': attendance.id, 'http_status': 201}
         except Exception as e:
             return {'status': 'error', 'message': str(e), 'http_status': 500}
 
+    def list_attendance(self, page=1, items_per_page=10):
+        attendances = self.factory.list_attendances(page, items_per_page)
+        attendances_list = [attendance.as_dict() for attendance in attendances]
+        return {'attendances': attendances_list, 'http_status': 200}
+
     def get_attendance(self, attendance_id):
-        attendance = self.db.attendance(attendance_id)
-        if attendance:
-            return {'attendance': attendance.as_dict(), 'http_status': 200}
+        attendance = self.factory.get_attendance(attendance_id)
+        if attendance is not None:
+            return {'attendance': attendance, 'http_status': 200}
         else:
             return {'status': 'error', 'message': 'Attendance not found', 'http_status': 404}
 
-    def list_attendance(self, page=1, items_per_page=10):
-        start = (page - 1) * items_per_page
-        end = page * items_per_page
-        attendances = self.db(self.db.attendance).select(orderby=self.db.attendance.id, limitby=(start, end))
-        if attendances:
-            # Convertir cada registro de estudiante a un diccionario y agregarlos a una lista
-            attendances_list = [attendance.as_dict() for attendance in attendances]
-            return {'attendances': attendances_list, 'http_status': 200}
-        else:
-            return {'status': 'error', 'message': 'No students found', 'http_status': 404}
-        
     def update_attendance(self, attendance_id, attendance_data):
-        attendance = self.db.attendance(attendance_id)
+        attendance = self.factory.update_attendance(attendance_id, attendance_data)
         if attendance:
-            attendance.update_record(**attendance_data)
-            self.db.commit()
             return {'status': 'success', 'message': 'Attendance updated successfully', 'http_status': 200}
         else:
             return {'status': 'error', 'message': 'Attendance not found', 'http_status': 404}
 
     def delete_attendance(self, attendance_id):
-        attendance = self.db.attendance(attendance_id)
-        if attendance:
-            self.db(self.db.attendance.id == attendance_id).delete()
-            self.db.commit()
-            return {'status': 'success', 'message': 'Attendance deleted successfully', 'http_status': 200}
-        else:
-            return {'status': 'error', 'message': 'Attendance not found', 'http_status': 404}
+        self.factory.delete_attendance(attendance_id)
+        return {'status': 'success', 'message': 'Attendance deleted successfully', 'http_status': 200}
--- applications/SIP/modules/services/api_services/api_students.py
+++ applications/SIP/modules/services/api_services/api_students.py
@@ -1,7 +1,8 @@
-
+from applications.SIP.modules.factory.students_factory import StudentFactory
 class APIStudent:    
     def __init__(self, db):
         self.db = db
+        self.factory = StudentFactory(self.db)
     
     def create_student(self, student_data):
         # Validación de los datos
@@ -9,48 +10,34 @@ class APIStudent:
             return {'status': 'error', 'message': 'Missing required fields', 'http_status': 400}
 
         try:
-            student_id = self.db.students.insert(**student_data)
-            self.db.commit()
-            return {'status': 'success', 'student_id': student_id, 'http_status': 201}
+            student = self.factory.get_or_create_student(student_data)
+            return {'status': 'success', 'student_id': student.id, 'http_status': 201}
         except Exception as e:
             return {'status': 'error', 'message': str(e), 'http_status': 500}
-
+        
     def list_student(self, page=1, items_per_page=10):
-        start = (page - 1) * items_per_page
-        end = page * items_per_page
-        students = self.db(self.db.students).select(orderby=self.db.students.id, limitby=(start, end))
-        if students:
-            # Convertir cada registro de estudiante a un diccionario y agregarlos a una lista
-            students_list = [student.as_dict() for student in students]
-            return {'students': students_list, 'http_status': 200}
-        else:
-            return {'status': 'error', 'message': 'No students found', 'http_status': 404}
+        students = self.factory.list_students(page, items_per_page)
+        students_list = [student.as_dict() for student in students]
+        return {'students': students_list, 'http_status': 200}
 
     def get_student(self, student_id):
-        student = self.db.students(student_id)
+        student = self.factory.get_student(student_id)
         if student:
             return {'student': student.as_dict(), 'http_status': 200}
         else:
             return {'status': 'error', 'message': 'Student not found', 'http_status': 404}
 
     def update_student(self, student_id, student_data):
-        student = self.db.students(student_id)
+        student = self.factory.update_student(student_id, student_data)
         if student:
-            student.update_record(**student_data)
-            self.db.commit()
             return {'status': 'success', 'message': 'Student updated successfully', 'http_status': 200}
         else:
             return {'status': 'error', 'message': 'Student not found', 'http_status': 404}
 
     def delete_student(self, student_id):
-        student = self.db.students(student_id)
-        if student:
-            self.db(self.db.students.id == student_id).delete()
-            self.db.commit()
-            return {'status': 'success', 'message': 'Student deleted successfully', 'http_status': 200}
-        else:
-            return {'status': 'error', 'message': 'Student not found', 'http_status': 404}
-        
+        self.factory.delete_student(student_id)
+        return {'status': 'success', 'message': 'Student deleted successfully', 'http_status': 200}
+
     def get_total_students(self):
         try:
             total_students = self.db(self.db.students.id > 0).count()
